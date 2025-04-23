8e11bb46660a53ab7114fe5343ca22dbcd7fff90
danjuan-77 <youngertutu@gmail.com>
2025-04-23 18:45:45 +0800

    11

--- inference.sh
+++ inference.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # video + audio
-# python inference.py --modal video_audio \
-#   --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B \
-#   --video_path ./data/test.mp4 \
-#   --instruct "Describe this video."
+python inference.py --modal video_audio \
+  --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B \
+  --video_path ./data/test.mp4 \
+  --instruct "Describe this video."
 
 # video only
 python inference.py --modal video \
-  --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B \
+  --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B \
   --video_path ./data/test.mp4 \
   --instruct "Describe this video."
 
 # audio only
 python inference.py --modal audio \
-  --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B \
+  --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B \
   --video_path ./data/test.mp4 \
   --instruct "Describe this video."
