c2260d612cf0d2fedf1f1973c66eb7536a8ea251
danjuan-77 <youngertutu@gmail.com>
2025-05-14 16:00:22 +0800

    update

--- eval0.5b_gpu2.sh
+++ eval0.5b_gpu2.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=2
 # HumanOmni-0.5B
 
-python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
-python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVQA
+python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_av
+python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-0.5B --task_path /share/nlp/tuwenming/projects//HAVIB/data/levels/level_6/AVSQA_v
 
 # nohup bash eval0.5b_gpu2.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_humanomni-0.5b_unimodal_gpu2_$(date +%Y%m%d%H%M%S).log 2>&1 &
--- eval_gpu1.sh
+++ eval_gpu1.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=1
 # HumanOmni-7B
 
-python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAH
-python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVC
+python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_av
+python eval.py --model_path /share/nlp/tuwenming/models/StarJiaxing/HumanOmni-7B --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_v
 
 # nohup bash eval_gpu1.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_humanomni-7b_unimodal_gpu1_$(date +%Y%m%d%H%M%S).log 2>&1 &
